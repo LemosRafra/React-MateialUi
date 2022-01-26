c49c45bd2ed7f75670832034d7c6e8f57145e9ab
Rafael <rafralemos@gmail.com>
2022-01-26 12:38:08 -0300

    Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,37 @@
 import React from "react";
-
-import Home from "./paginas/home/Home";
-
-import Navbar from "./components/estatics/navbar/Navbar";
-
-import Footer from "./components/estatics/footer/Footer";
-
-import Login from "./paginas/login/Login";
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import Navbar from "./components/estatics/navbar/Navbar";
+import Footer from "./components/estatics/footer/Footer";
+import CadastroUsuario from "./paginas/cadastro/CadastroUsuario";
+import Home from "./paginas/home/Home";
+import Login from "./paginas/login/Login";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar></Navbar>
-        <Switch>
-          <div style={{ minHeight: "100vh" }}>
-            <Route exact path="/">
-              <Login></Login>
-            </Route>
-            <Route path="/login">
-              <Login></Login>
-            </Route>
-            <Route path="/home">
-              <Home></Home>
-            </Route>
-          </div>
-        </Switch>
-        <Footer></Footer>
-      </Router>
-    </>
+    <Router>
+      <Navbar />
+      <Switch>
+        <div style={{ minHeight: "100vh" }}>
+          <Route exact path="/">
+            <Login />
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/home">
+            <Home />
+          </Route>
+
+          <Route path="/cadastrousuario">
+            <CadastroUsuario />
+          </Route>
+        </div>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
